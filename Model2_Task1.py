386af36b74afe63a7b7f638cf11d667c57ade558
Atif <at454507@dal.ca>
2019-02-07 07:29:58 +0000

    Now everything is printing fine in their respective columns except for one issue. At some places there is an empty index like ''. which is due to the fact that we have ,, at some places in original file. I will continue to work on other tasks and will come back to this one later. I just have to find a way to remove one comma from each set of two commas.

--- Model2_Task1.py
+++ Model2_Task1.py
@@ -1,3 +1,5 @@
+import string
+
 def street():
 	l_strname = []
 	l_fullname = []
@@ -5,16 +7,19 @@ def street():
 	l_tostreet = []
 	l = []
 	t = ()
+	import string
 
 	fout = open('Street_Centrelines.csv')
 	for line in fout:
-		line1 = line.split()
-		l_strname.append(line1[2:3])
-		l_fullname.append(line1[4:5])
-		l_frmstreet.append(line1[6:7])
-		l_tostreet.append(line1[7:8])
+		line1 = line.split(',')
+		#line2 = line.strip(string.punctuation + string.whitespace)
+		l_strname.extend(line1[2:3])
+		l_fullname.extend(line1[4:5])
+		l_frmstreet.extend(line1[6:7])
+		l_tostreet.extend(line1[7:8])
 
 
+	#print(l_tostreet)
 	t = zip(l_strname,l_fullname,l_frmstreet,l_tostreet)
 	for string in t:
 		l.append(string)
