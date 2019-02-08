d33af71802638afec895d764ad47d8c42c171e03
Atif <at454507@dal.ca>
2019-02-08 01:59:40 +0000

    Task2_Subtask2 has been completed. It shows the total number of Non-Standard bus stops that are in local streets.

--- Model2_Task2.py
+++ Model2_Task2.py
@@ -4,7 +4,9 @@ def bus():
 	l_strclass = []
 	l = []
 	l1 = []
+	l2 = []
 	c = 0
+	c1 = 0
 	fout = open('Bus_Stops.csv')
 	for line in fout:
 		line1 = line.split(',')
@@ -25,13 +27,23 @@ def bus():
 		l.append(l_strclass[i])
 
 
-	print(type(l[4]))
+
 	for i in range(len(l)-2):
 		if l[i+1] == ['Accessible']:
 			if l[i+2] == ['ARTERIAL']:
 				l1.append(l[i])
 				c = c + 1
-	print(l1)
-	print("Total number of Arterial bus stops = ",c)
 
+	for i in range(len(l)-2):
+		if l[i+1] == ['Non-Standard']:
+			if l[i+2] == ['LOCAL STREET']:
+				l2.append(l[i])
+				c1 = c1 + 1
+
+
+	#print(l1)
+	#print("Total number of Arterial bus stops = ",c)
+
+	print(l2)
+	print("Total number of Non-Standard Local Streets = ",c1)
 bus()
